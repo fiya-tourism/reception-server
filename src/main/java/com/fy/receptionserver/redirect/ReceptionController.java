2ec91e8b2088d3b4aa1e19b3840ee01d713fc116
My XuZhen <515897959@qq.com>
2019-07-18 17:48:15 +0800

    分包结构，添加feign注解

--- src/main/java/com/fy/receptionserver/redirect/ReceptionController.java
+++ src/main/java/com/fy/receptionserver/redirect/ReceptionController.java
@@ -1,4 +1,4 @@
-package com.fy.receptionserver.controller;
+package com.fy.receptionserver.redirect;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
